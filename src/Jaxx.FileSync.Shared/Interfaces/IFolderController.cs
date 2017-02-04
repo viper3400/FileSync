0b211cb89588660580dd8a93f378f43d1e772dae
viper3400 <viper_3400@gmx.de>
2017-02-04 15:42:27 +0100

    added IFolderController interface

---
+++ src/Jaxx.FileSync.Shared/Interfaces/IFolderController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jaxx.FileSync.Shared.Interfaces
+{
+    interface IFolderController
+    {
+        bool CreateFolder(string name, string parentFolder);
+        bool DeleteFolder(string name);
+    }
+}
