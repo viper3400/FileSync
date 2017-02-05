8dca4a923f8ad10e2a2b7edcb8fb4fd984403b1a
viper3400 <viper_3400@gmx.de>
2017-02-05 14:43:12 +0100

    implemented dummy DeleteAgedFilesMethod

--- src/Jaxx.FileSync.GoogleDrive/GoogleDriverDeleteController.cs
+++ src/Jaxx.FileSync.GoogleDrive/GoogleDriverDeleteController.cs
@@ -1,6 +1,7 @@
 ﻿using Google.Apis.Drive.v3;
 using Jaxx.FileSync.Shared.Interfaces;
 using System;
+using System.Globalization;
 
 namespace Jaxx.FileSync.GoogleDrive
 {
@@ -15,7 +16,18 @@ namespace Jaxx.FileSync.GoogleDrive
 
         public bool DeleteAgedFiles(int fileAgeInDays, string folder)
         {
-            throw new NotImplementedException();
+            var time = DateTime.Now - new TimeSpan(fileAgeInDays, 0, 0, 0, 0);
+            
+            string timeString = time.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            //var files = DriveApi.GetFiles(_service, $"modifiedTime <= '{timeString}' and '{folder}' in parents");
+            var files = DriveApi.GetFiles(_service, $"modifiedTime <= '{timeString}'");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"This would delete {file.Name}, last modified on {file.ModifiedTime.Value}");                
+            }
+
+            return true;
         }
     }
 }
